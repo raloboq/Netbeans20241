99a013d5c1275a9c6846fa4961bb5d23e52f04d0
usuario <usuario@EDI102DOC>
2024-05-10 12:45:49 -0500

    Primer commit se muestran 3 mensajes

---
+++ EjemploGit/src/ejemplogit/EjemploGit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplogit;
+
+/**
+ *
+ * @author usuario
+ */
+public class EjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola Github :)");
+        System.out.println("otra cosa");
+        System.out.println("algo mas");
+        int b = 5;
+    }
+    
+}
